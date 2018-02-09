068d2dea64d07c86363ea159322b4ad2c22f6277
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2018-02-09 21:35:38 +0800

    Improve postinstall

---
+++ postinstall.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let cp = require('child_process');
+let path = require('path');
+
+let bowerBinDir = path.join(path.dirname(require.resolve('bower')), "../bin")
+cp.execSync('bower install', {env: {PATH: process.env.PATH + ":" + bowerBinDir}});
