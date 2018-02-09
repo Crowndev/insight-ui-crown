7c84947f6f05d2e93e6e4e9f1b8ec92f789b3fcd
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2018-02-09 21:36:53 +0800

    Fix syntax

--- postinstall.js
+++ postinstall.js
@@ -3,5 +3,5 @@
 let cp = require('child_process');
 let path = require('path');
 
-let bowerBinDir = path.join(path.dirname(require.resolve('bower')), "../bin")
-cp.execSync('bower install', {env: {PATH: process.env.PATH + ":" + bowerBinDir}});
+let bowerBinDir = path.join(path.dirname(require.resolve('bower')), '../bin');
+cp.execSync('bower install', {env: {PATH: process.env.PATH + ':' + bowerBinDir}});
